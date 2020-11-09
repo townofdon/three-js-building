d5ed46f17fd90cec1391d3252b66e864b9b4d788
Don Townsend <don@teamairship.com>
2020-11-08 23:06:03 -0500

    display 3d model (broken blender version)

--- src/scenes/BuildingScene.tsx
+++ src/scenes/BuildingScene.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import { Canvas } from 'react-three-fiber';
-import { RotatingBox } from '../components/RotatingBox';
+// import { RotatingBox } from '../components/3DObjects/RotatingBox';
+import TestHouse from '../components/3DObjects/TestHouse';
 
 export const BuildingScene = () => {
   return (
     <Canvas>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <RotatingBox position={[-1.2, 0, 0]} />
-      <RotatingBox position={[1.2, 0, 0]} />
+      {/* <RotatingBox position={[-1.2, 0, 0]} />
+      <RotatingBox position={[1.2, 0, 0]} /> */}
+      <TestHouse />
     </Canvas>
   );
 };
